5d67774adf2ea757a3d78837afbaded290351b3c
César Magalhães <ceanma@gmail.com>
2015-04-10 09:06:37 -0300

    Foi feito o exemplo 'Spring DI via constructor', Acesse o tutorial atraves do link: http://www.mkyong.com/spring/spring-di-via-constructor/

--- spring/spring-core/spring-core-tutorial1/src/main/java/com/mkyong/output/OutputHelper.java
+++ spring/spring-core/spring-core-tutorial1/src/main/java/com/mkyong/output/OutputHelper.java
@@ -3,10 +3,21 @@ package com.mkyong.output;
 public class OutputHelper {
 	private IOutputGenerator outputGenerator;
 
+	public OutputHelper() {
+		super();
+	}
+
+	// DI via constructor
+	public OutputHelper(IOutputGenerator outputGenerator) {
+		this();
+		setOutputGenerator(outputGenerator);
+	}
+
 	public void generateOutput() {
 		outputGenerator.generateOutput();
 	}
 
+	// DI via set method
 	public void setOutputGenerator(IOutputGenerator outputGenerator) {
 		this.outputGenerator = outputGenerator;
 	}

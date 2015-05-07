401b9c17e26b3ffab0c5a7a795a7f2c622fea257
César Magalhães <ceanma@gmail.com>
2015-05-07 14:07:54 -0300

    Foi testado o exemplo 'Spring Batch Example - MySQL Database To XML', acesse atraves da url: http://www.mkyong.com/spring-batch/spring-batch-example-mysql-database-to-xml/

---
+++ spring/spring-batch/Spring-Batch-Example_H2-Database-To-XML/src/main/resources/dml.sql
@@ -0,0 +1,6 @@
+--Data manipulation language
+insert into USERS(USER_LOGIN, USER_PASS, AGE) values ('cesar', 'C&s@r12345', 32);
+insert into USERS(USER_LOGIN, USER_PASS, AGE) values ('leone', 'L&#n&12345', 30);
+insert into USERS(USER_LOGIN, USER_PASS, AGE) values ('leandro', 'L&@ndr#12345', 29);
+insert into USERS(USER_LOGIN, USER_PASS, AGE) values ('daniel', 'D@ni&!12345', 24);
+insert into USERS(USER_LOGIN, USER_PASS, AGE) values ('cintia', 'Cinti@12345', 34);

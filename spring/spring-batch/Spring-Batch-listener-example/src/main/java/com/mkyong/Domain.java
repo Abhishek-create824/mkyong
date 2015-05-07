e187d48b1da4f52adbbb5d28f62f25509f51809c
César Magalhães <ceanma@gmail.com>
2015-05-07 16:35:16 -0300

    Foi testado o exemplo 'Spring Batch listeners example', acesse atraves da url: http://www.mkyong.com/spring-batch/spring-batch-listeners-example/

---
+++ spring/spring-batch/Spring-Batch-listener-example/src/main/java/com/mkyong/Domain.java
@@ -0,0 +1,24 @@
+package com.mkyong;
+
+public class Domain {
+
+	private int id;
+	private String domain;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDomain() {
+		return domain;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+
+}

fbb570d1ffe5607eb8e2ab4b3d914b7d9b04c489
César Magalhães <ceanma@gmail.com>
2015-05-12 11:04:41 -0300

    Foi testado o exemplo 'Spring MVC handling multipage forms with AbstractWizardFormController', acesse atraves da url: http://www.mkyong.com/spring-mvc/spring-mvc-handling-multipage-forms-with-abstractwizardformcontroller/

---
+++ spring/spring-mvc/Handling-Multipage-Wizard-Forms/src/main/java/com/mkyong/common/model/User.java
@@ -0,0 +1,28 @@
+package com.mkyong.common.model;
+
+public class User{
+	
+	private String userName;
+	private String password;
+	private String remark;
+	
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
---
+++ spring/spring-mvc/Handling-Multipage-Wizard-Forms/src/main/webapp/WEB-INF/pages/WelcomePage.jsp
@@ -0,0 +1,6 @@
+<html>
+	<body>
+		<h2>Handling multipage forms in Spring MVC</h2>
+		Click here to start playing - <a href="user.htm">AbstractWizardFormController example</a>
+	</body>
+</html>
